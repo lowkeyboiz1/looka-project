48788d3bafab4a6d56659019d03e557bf57faa0b
unknown <khangluong2002@gmail.com>
2023-05-06 22:34:52 +0700

    add comps

--- src/components/GetStartFlex/index.js
+++ src/components/GetStartFlex/index.js
@@ -1,28 +1,28 @@
 import Button from '../Button';
 import classNames from 'classnames/bind';
-import styles from './GetStart.module.scss';
+import styles from './GetStartFlex.module.scss';
 
 const cx = classNames.bind(styles);
 
-function GetStart({ title, desc, className, classNameInput, fontText }) {
+function GetStartFlex({ title, desc, className, classNameInput, fontText, black }) {
     return (
-        <div className={`wrapper px-[2.4rem] py-[8.8rem] text-center pt-[134px] ${className}`}>
-            <div className="">
+        <div className={`wrapper px-[2.4rem] py-[8.8rem] pb-[6rem] flex ${className}`}>
+            <div className="w-full pl-[5rem]">
                 <div
-                    className={`title text-[var(--text-color)] font-bold ${
+                    className={`title text-[#fff] font-bold md:w-[46.4rem] ${
                         fontText ? 'text-[3rem] md:text-[4rem]' : 'text-[3.6rem] md:text-[4.8rem]'
                     } leading-normal`}
                 >
                     {title}
                 </div>
-                <div className="desc text-[#676e8b] text-[20px] my-[32px] font-normal">{desc}</div>
+                <div className="desc text-[#fff] text-[1.6rem] my-[32px] font-normal md:w-[46.4rem]">{desc}</div>
             </div>
-            <div className="control md:flex items-center justify-center">
+            <div className="control md:flex items-center justify-center md:w-full">
                 <input
                     className={cx('input', `md:max-w-[424px] ${classNameInput && '!bg-white'}`)}
                     placeholder="Enter your company name"
                 />
-                <Button primary large className={'md:ml-2 w-full md:w-auto mt-3 md:mt-0'}>
+                <Button black large className={'md:ml-2 w-full md:w-auto mt-3 md:mt-0'}>
                     Get started
                 </Button>
             </div>
@@ -30,4 +30,4 @@ function GetStart({ title, desc, className, classNameInput, fontText }) {
     );
 }
 
-export default GetStart;
+export default GetStartFlex;
